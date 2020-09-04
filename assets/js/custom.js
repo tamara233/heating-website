cbe8f5aef7593faa0b10efb3c04a393164b45b68
Tamara Sirakanyan <tamarasirakanyan@tamsmac.local>
2020-09-04 14:03:31 +0400

    corrected alignments, menu, responsive sizes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,6 +2,21 @@
 $('.panel-heading').on('click', function(){
     $(this).children('.panel-title').toggleClass('faq-shadow');
     $(this).children('.panel-title').toggleClass('no-hover');
+    $(this).find(".chevron").toggleClass('rotate-90');
 
     $(this).closest('.panel-default').toggleClass('faq-shadow');
 });
+
+//add box-shadow to the navbar
+if ($(window).width() > 992) {
+    $('.navbar').addClass('sticky-top')
+    $(window).scroll(function() {     
+        var scroll = $(window).scrollTop();
+        if (scroll > 0) {
+            $(".navbar").addClass("navbar-shadow");
+        }
+        else {
+            $(".navbar").removeClass("navbar-shadow");
+        }
+    });
+}
