45b546846ccb8fba27b28ad52cdcfce918b65fc2
Tamara Sirakanyan <tamarasirakanyan@tamsmac.local>
2020-09-07 12:43:34 +0400

    Updated validation text for Bis form

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -20,3 +20,28 @@ if ($(window).width() > 992) {
         }
     });
 }
+
+// $('form').on('submit', function(e){
+//     // validation code here
+   
+//       e.preventDefault();
+    
+//   });
+
+  window.addEventListener('load', function() {
+    var forms = document.getElementsByClassName('needs-validation');
+    var validateGroup = document.getElementsByClassName('to-validate');
+
+    var validation = Array.prototype.filter.call(forms, function(form) {
+    form.addEventListener('submit', function(event) {
+    if (form.checkValidity() === false) {
+    event.preventDefault();
+    event.stopPropagation();    
+    }
+    //Added validation class to all form-groups in need of validation
+    for (var i = 0; i < validateGroup.length; i++) {
+        validateGroup[i].classList.add('was-validated');
+    }   
+    }, false);
+    });
+    }, false);
